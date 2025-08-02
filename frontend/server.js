d55b91e7d4729e7f2f3fcf654ddd608a2ea42e9a
Shiva Garg <shiva19garg@gmail.com>
2025-08-02 18:05:09 +0530

    updated

---
+++ frontend/server.js
@@ -0,0 +1,21 @@
+var express = require ("express");
+let app = express();
+app.use (express.static("landing"))
+
+app.listen(2025,function(req,resp){
+    console.log("Server Has Been Started :)");
+})
+//index
+app.get("/",function(req,resp){
+    let path=__dirname+"index.html";
+    resp.sendFile(path);
+})
+//readme
+app.get("/readme",function(req,resp){
+    let path=__dirname+"/landing/readme.html";
+    resp.sendFile(path);
+})
+app.get("/app",function(req,resp){
+    let path=__dirname+"/landing/app/auth/login/index.html";
+    resp.sendFile(path);
+})
